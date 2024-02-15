963638281c9017bdd52ebf5511f470b297546990
Jadd Clareth Gonzalez Quintero <clareth.gonzalez@sistemasgyg.com.co>
2024-02-15 15:45:40 -0500

    first commit

---
+++ AlgoritmosApi/AlgoritmosApi/Controllers/Multiplode3.cs
@@ -0,0 +1,54 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+ 
+namespace AlgoritmosApi.Controllers
+{
+
+	[ApiController]
+	[Route("[controller]")]
+
+	public class Multiplode3 : ControllerBase
+	{
+		[HttpGet("{num}")]
+		public IActionResult Get([Required] int num)
+		{
+			string multiplo = "";
+			string intervalo = "";
+			if (num > 0)
+			{
+				if (num >= 99 && num <= 201)
+				{
+					if (num % 3 == 0)
+					{
+						multiplo = "es multilo de 3";
+						intervalo = "si esta entre 100 y 200";
+					}
+					else
+					{
+						multiplo = "no es multilo de 3";
+						intervalo = "si esta entre 100 y 200";
+					}
+				}
+				else
+				{
+					if (num % 3 == 0)
+					{
+						multiplo = "es múltiplo de 3";
+						intervalo = "no esta entre 100 y 200";
+					}
+					else
+					{
+						multiplo = "no es múltiplo de 3";
+						intervalo = "no esta entre 100 y 200";
+					}
+				}
+				return Content($"El numero {num} {multiplo} y {intervalo}");
+			}
+			else
+			{
+				return BadRequest($"El valor {num} no corresponde a un Entero");
+			}
+		}
+	}
+}
---
+++ AlgoritmosApi/AlgoritmosApi/Controllers/Promnotas.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace AlgoritmosApi.Controllers
+
+{
+
+	[ApiController] //Indica que la clase es un controlador de API
+
+	[Route("[controller]")] //Indica la ruta del controlador
+
+	public class Promnotas : ControllerBase //Controlbase convierte a controlador MVC y API
+
+	{
+
+		public double PromedioNotas; //Se define la variable
+
+		[HttpPost("promedioNotas")] //Nombre que va a recibir parte de la url
+
+		public IActionResult Post([Required] int num1, [Required] int num2, [Required] int num3)
+
+		// Pilas con los tipo de Dato
+
+		{
+
+			if (num1 > 0 && num2 > 0 && num3 > 0)
+
+			{
+
+				double PromTotal = (num1 + num2 + num3) / 3.0; // Calcular el promedio
+
+				PromTotal = Math.Round(PromTotal, 2); // Redondear el promedio a 2 decimales
+
+				return Content($"El promedio de las notas es: {PromTotal}"); // Mostrar el promedio
+
+			}
+
+			else
+
+			{
+
+				return Content("Los valores digitados deben ser positivos");
+
+			}
+
+		}
+
+		//Se crea metodo que realice el promedio de las 3 notas
+
+		/*public double PromedioNotasTotal(int num1, int num2, int num3)
+
+        {
+
+            //Al estar dentro de un metodo que ya contiene un private/public no se vuelve a poner adentro
+
+            double PromTotal = (num1 + num2 + num3) / 3.0;
+
+            return PromTotal;   
+
+        }*/
+
+	}
+
+}
